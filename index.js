eabcd86c732cf05b2cfdd0935faabd8d7ad1ad1c
James Trickey <james.trickey@velocityapp.com>
2019-08-14 17:26:02 +0100

    - removed mapping for android as no longer needed

--- index.js
+++ index.js
@@ -3,22 +3,11 @@ import { NativeModules, Platform } from 'react-native';
 const isiOS = Platform.OS === 'ios'
 const { RNSoundDetection } = NativeModules;
 
-const getAudioTrack = (type) => {
-  if (audioTrack) {
-    return { ...audioTrack, type }
-  }
-  return null
-}
-
 const SoundDetection = isiOS ? RNSoundDetection : { 
   getTracks: (url) => {
     return new Promise((r) => {
       RNSoundDetection.getTracks(url, (tracks) => {
-        let result = tracks
-        if (result) {
-          result = JSON.parse(tracks).map(getAudioTrack)
-        }
-        r(result || [])
+        r(tracks ?  JSON.parse(tracks) : []);
       })
     })
   } 
