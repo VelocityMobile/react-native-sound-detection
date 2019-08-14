4a2ca5616a2c88192751e58a3ecd03a77e006c4f
James Trickey <james.trickey@velocityapp.com>
2019-08-14 17:03:48 +0100

    - for android, due to change in object forma we now just pass the type

--- index.js
+++ index.js
@@ -3,23 +3,24 @@ import { NativeModules, Platform } from 'react-native';
 const isiOS = Platform.OS === 'ios'
 const { RNSoundDetection } = NativeModules;
 
-const getAudioTrack = (track) => {
-  const audioTrack = track && track.mFormat && track.mFormat.mMap
+const getAudioTrack = (type) => {
   if (audioTrack) {
-    return { ...audioTrack, type: audioTrack.mime.includes('audio') ? 'audio' : 'video' }
+    return { ...audioTrack, type }
   }
   return null
 }
 
-const SoundDetection = isiOS ? RNSoundDetection : { getTracks: (url) => {
-  return new Promise((r) => {
-    RNSoundDetection.getTracks(url, (tracks) => {
-      let result = tracks
-      if (result) {
-        result = JSON.parse(tracks).map(getAudioTrack)
-      }
-      r(result || [])
+const SoundDetection = isiOS ? RNSoundDetection : { 
+  getTracks: (url) => {
+    return new Promise((r) => {
+      RNSoundDetection.getTracks(url, (tracks) => {
+        let result = tracks
+        if (result) {
+          result = JSON.parse(tracks).map(getAudioTrack)
+        }
+        r(result || [])
+      })
     })
-  })
-} }
+  } 
+}
 export default SoundDetection;
